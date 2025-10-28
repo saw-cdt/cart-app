554f744441c429a2c811a6b2647b5d2a56d0e55e
Alex <alejandro.quintanillaea@gmail.com>
2025-10-28 03:08:51 -0600

    added shopping cart structure

--- src/CartApp.jsx
+++ src/CartApp.jsx
@@ -1,3 +1,4 @@
+import { CartView } from "./components/CartView";
 import { CatalogView } from "./components/CatalogView";
 
 export const CartApp = () => {
@@ -5,7 +6,12 @@ export const CartApp = () => {
     <>
       <div className="container">
         <h3>Cart App</h3>
+
         <CatalogView />
+
+        <div className="my-4 w-50">
+          <CartView />
+        </div>
       </div>
     </>
   );
