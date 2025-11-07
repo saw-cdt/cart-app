0acaa4114f32de4c8cc0da61101b7be2f87990db
Alex <alejandro.quintanillaea@gmail.com>
2025-11-06 23:43:32 -0600

    Added Routes

--- src/CartApp.jsx
+++ src/CartApp.jsx
@@ -1,6 +1,8 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import { CartView } from "./components/CartView";
 import { CatalogView } from "./components/CatalogView";
 import { useItemsCart } from "./hooks/useItemsCart";
+import { Navbar } from "./components/Navbar";
 
 export const CartApp = () => {
   const { cartItems, handlerAppProductCart, handlerDeleteProduct } =
@@ -10,14 +12,31 @@ export const CartApp = () => {
     <>
       <div className="container my-4">
         <h3>Cart App</h3>
-
-        <CatalogView handler={handlerAppProductCart} />
-
-        {cartItems?.length <= 0 || (
-          <div className="my-4 w-50">
-            <CartView items={cartItems} handlerDelete={handlerDeleteProduct} />
-          </div>
-        )}
+        <Navbar />
+        <Routes>
+          <Route
+            path="catalog"
+            element={<CatalogView handler={handlerAppProductCart} />}
+          />
+          <Route
+            path="cart"
+            element={
+              cartItems?.length <= 0 ? (
+                <div className="alert alert-warning">
+                  The Shopping cart is empty!
+                </div>
+              ) : (
+                <div className="my-4 w-50">
+                  <CartView
+                    items={cartItems}
+                    handlerDelete={handlerDeleteProduct}
+                  />
+                </div>
+              )
+            }
+          />
+          <Route path="/" element={<Navigate to={"/catalog"} />} />
+        </Routes>
       </div>
     </>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+export const Navbar = () => {
+  return (
+    <>
+      <ul class="nav nav-tabs">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">
+            Home
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Catalog
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Cart
+          </a>
+        </li>
+      </ul>
+    </>
+  );
+};
